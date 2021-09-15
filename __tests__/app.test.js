ecf8a8aa975f734eca454e63f2cdd58d7114a93a
Chase <cabbott93@gmail.com>
2021-09-15 11:30:37 -0700

    mod tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,21 +2,12 @@ const mongoose = require('mongoose')
 const { User } = require('../connection.js')
 
 describe('api routes', () => {
-  beforeEach(() => {
-    return User.deleteMany({})
-  })
+  // beforeEach(() => {
+  //   return User.deleteMany({})
+  // })
   // afterEach(() => {
   //   mongoose.disconnect()
   // })
   it('signs a user up to the database', async () => {
-    const user = {
-      username: 'chase',
-      password: 'password'
-    }
-    // const newUser = await User.findOne({ username: 'chase' })
-    // console.log(newUser)
-
-    // expect(newUer).toEqual({ _id: expect.any(String), ...user })
-
   })
 })
