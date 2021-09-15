89da02022150a270387689437a75d0dce9bad7dd
Chase <cabbott93@gmail.com>
2021-09-15 11:43:31 -0700

    add yml

--- Schema/typeDefs.js
+++ Schema/typeDefs.js
@@ -6,19 +6,31 @@ type User {
   username: String!
 }
 
+type Todo {
+  _id: String!
+  userId: String!
+  content: String!
+  completed: Boolean!
+}
+
 #Queries
 #Where all of our queries in our application live
 type Query {
   #Returns a list of users
   getAllUsers: [User!]!
+
   #getMatchingUser
   user(username: String!): User
+
+  #Returns all of a user's todos
+  todo(username: String!): [Todo!]!
 }
 
 #Mutations
   type Mutation {
     login(username: String!, password: String!): User!
     signup(username: String!, password: String!): User!
+    addTodo(userId: String!, content: String!): Todo!
   }
 `
 
